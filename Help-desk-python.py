e625deb18fe06101dd358e283504b6eeed962895
Syeda20231853 <145252418+Syeda20231853@users.noreply.github.com>
2023-09-17 19:45:54 +1200

    Update Help-desk-python.py
    python code for help desk

--- Help-desk-python.py
+++ Help-desk-python.py
@@ -1,1 +1,59 @@
+#!/usr/bin/python
 
+# Printing First line for Help Desk System Prototype Python Script/Programe
+print('Help Desk Ticketing System Prototype!')
+
+#Taking Information as Input for Ticket Creation
+Staff-ID = input("Staff ID: ")
+Ticket-creator-name = input("Ticket creator name: ")
+Contact-email = input("Contact email: ")
+Ticket-desc = input("Description of the issue: ")
+
+#TicketID Generation
+Ticket-Id = 2000
+def count()
+    global Ticket-Id
+    Ticket-Id += 1
+    print Ticket-Id
+
+#Feedback Response
+Response = input("Do you want to provide feedback Y or N: ")
+if Resposne = N
+    then
+     Response = "No Yet Provided"
+
+#Ticket-Status
+Ticket-Status = 'Open'
+def switch(Ticket-Status):
+    if Ticket-Status == "Open":
+        return "Ticket in Open State."
+    elif Ticket-Status == "closed":
+        return "Ticket in closed State."
+    elif Ticket-Status == "ReOpen":
+        return "Ticket in ReOpen State."
+
+#Response to ticket for Password Change Module
+If
+index = ticket-desc.find ("Password Change") 
+then 
+def splitting(Staff-ID)
+ list1 =  Staff-ID.split[:2] 
+def splitting(Ticket-creator-name)
+list2 =  Ticket-creator-name.split[:3]
+def joining (list)
+list = list1.join(list2)
+print('Password Changed is : ',list')
+ and
+Ticket-Id -= 1
+and
+Ticket-status='closed'
+print("Ticket closed")
+ 
+
+#Displaying the ticket/Output
+print("Ticket Number:" ('Ticket-Id'))
+print("Ticket Creator:" ('Ticket-creator-name'))
+print("Staff-ID:" ('Staff-ID'))
+print("Email Address:" ('Contact-email'))
+print("Response:" ('Response'))
+print("Ticket Status:" ('Ticket-Status'))      
